4f63b4a9a9ecef73a04d930284705c6762ae240c
Lik-i-l <nik28092000@yandex.ru>
2026-05-22 11:36:13 +0300

    Update math.ts

--- math.ts
+++ math.ts
@@ -1,14 +1,13 @@
-const b: number = 2.5;
-const xValues: number[] = [1.28, 3.28, 0.4, 1.1, 2.4, 3.6, 1.7, 3.9];
-
-function f(x: number, b: number): number {
+export function f(x: number, b: number): number {
     const inner: number = b ** 3 + x ** 3;
     const numerator: number = 1 + Math.sin(inner) ** 2;
     const denominator: number = Math.cbrt(inner);
     return numerator / denominator;
 }
 
+// Запуск программы
+const b: number = 2.5;
+const xValues: number[] = [1.28, 3.28, 0.4, 1.1, 2.4, 3.6, 1.7, 3.9];
 for (const x of xValues) {
-    const y: number = f(x, b);
-    console.log(`x = ${x}, y = ${y.toFixed(2)}`);
+    console.log(`x = ${x}, y = ${f(x, b).toFixed(2)}`);
 }
